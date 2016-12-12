e649794a702bf1b19af4a27fb3d6ad57cfd6c83c
Guorong Feng <wifeng@microsoft.com>
2016-12-12 11:33:35 +0800

    check in file to repos

---
+++ MudOnSF/FacilityService.Interface/Enum/PlayerStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacilityService.Interface.Enum
+{
+    public enum PlayerStatus
+    {
+        login,
+        quit,
+        idle,
+        NotExist,
+    }
+}
